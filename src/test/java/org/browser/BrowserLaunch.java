1a1e3a1907d418e4617e5536d6157964cd04760d
Rammohan4 <crammohan55@gmail.com>
2024-07-05 13:03:22 +0530

    id to name

--- src/test/java/org/browser/BrowserLaunch.java
+++ src/test/java/org/browser/BrowserLaunch.java
@@ -16,6 +16,8 @@ public static void main(String[] args) {
 	driver.get("https://www.facebook.com/");
 	driver.manage().window().maximize();
 	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	System.out.println("Test1");
+	System.out.println("Test2");
 	WebElement txtUser = driver.findElement(By.name("email"));
 	txtUser.sendKeys("Revathi");
 	WebElement txtpass = driver.findElement(By.id("pass"));
